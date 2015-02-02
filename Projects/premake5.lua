9b7bfcaacab95d691bd6318e924eb07e320ed335
Daniel <danielga@outlook.pt>
2015-02-02 23:23:04 +0000

    Added memoverride. Added headers to vpaths.

--- Projects/premake5.lua
+++ Projects/premake5.lua
@@ -27,7 +27,11 @@ solution("gm_concommandx")
 
 	project("gmsv_concommandx")
 		kind("SharedLib")
-		defines({"GMMODULE", "CONCOMMANDX_SERVER"})
+		defines({
+			"GMMODULE",
+			"CONCOMMANDX_SERVER",
+			"SUPPRESS_INVALID_PARAMETER_NO_INFO"
+		})
 		includedirs({
 			SOURCE_FOLDER,
 			GARRYSMOD_MODULE_BASE_FOLDER .. "/include",
@@ -36,8 +40,18 @@ solution("gm_concommandx")
 			SDK_FOLDER .. "/public/tier0",
 			SDK_FOLDER .. "/public/tier1"
 		})
-		files({SOURCE_FOLDER .. "/*.cpp"})
-		vpaths({["Source files"] = SOURCE_FOLDER .. "/**.cpp"})
+		files({
+			SOURCE_FOLDER .. "/*.h",
+			SOURCE_FOLDER .. "/*.cpp",
+			SDK_FOLDER .. "/public/tier0/memoverride.cpp"
+		})
+		vpaths({
+			["Header files"] = SOURCE_FOLDER .. "/**.h",
+			["Source files"] = {
+				SOURCE_FOLDER .. "/**.cpp",
+				SDK_FOLDER .. "/**.cpp"
+			}
+		})
 
 		targetprefix("")
 		targetextension(".dll")
@@ -66,7 +80,11 @@ solution("gm_concommandx")
 
 	project("gmcl_concommandx")
 		kind("SharedLib")
-		defines({"GMMODULE", "CONCOMMANDX_CLIENT"})
+		defines({
+			"GMMODULE",
+			"CONCOMMANDX_CLIENT",
+			"SUPPRESS_INVALID_PARAMETER_NO_INFO"
+		})
 		includedirs({
 			SOURCE_FOLDER,
 			GARRYSMOD_MODULE_BASE_FOLDER .. "/include",
@@ -75,8 +93,18 @@ solution("gm_concommandx")
 			SDK_FOLDER .. "/public/tier0",
 			SDK_FOLDER .. "/public/tier1"
 		})
-		files({SOURCE_FOLDER .. "/*.cpp"})
-		vpaths({["Source files"] = SOURCE_FOLDER .. "/**.cpp"})
+		files({
+			SOURCE_FOLDER .. "/*.h",
+			SOURCE_FOLDER .. "/*.cpp",
+			SDK_FOLDER .. "/public/tier0/memoverride.cpp"
+		})
+		vpaths({
+			["Header files"] = SOURCE_FOLDER .. "/**.h",
+			["Source files"] = {
+				SOURCE_FOLDER .. "/**.cpp",
+				SDK_FOLDER .. "/**.cpp"
+			}
+		})
 
 		targetprefix("")
 		targetextension(".dll")
