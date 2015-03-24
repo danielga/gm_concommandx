a0b5f6bfd1b5f7120642c5287df3b18e9bae094d
Daniel <danielga@outlook.pt>
2015-03-24 20:14:54 +0000

    Lowercased filepaths. Updated gmod-module-base submodule.

--- projects/premake5.lua
+++ projects/premake5.lua
@@ -1,6 +1,6 @@
 SDK_FOLDER = "E:/Programming/source-sdk-2013/mp/src"
 GARRYSMOD_MODULE_BASE_FOLDER = "../gmod-module-base"
-SOURCE_FOLDER = "../Source"
+SOURCE_FOLDER = "../source"
 PROJECT_FOLDER = os.get() .. "/" .. _ACTION
 
 solution("gm_concommandx")
@@ -17,13 +17,13 @@ solution("gm_concommandx")
 	filter("configurations:Release")
 		optimize("On")
 		vectorextensions("SSE2")
-		objdir(PROJECT_FOLDER .. "/Intermediate")
-		targetdir(PROJECT_FOLDER .. "/Release")
+		objdir(PROJECT_FOLDER .. "/intermediate")
+		targetdir(PROJECT_FOLDER .. "/release")
 
-	filter({"configurations:Debug"})
+	filter("configurations:Debug")
 		flags({"Symbols"})
-		objdir(PROJECT_FOLDER .. "/Intermediate")
-		targetdir(PROJECT_FOLDER .. "/Debug")
+		objdir(PROJECT_FOLDER .. "/intermediate")
+		targetdir(PROJECT_FOLDER .. "/debug")
 
 	project("gmsv_concommandx")
 		kind("SharedLib")
